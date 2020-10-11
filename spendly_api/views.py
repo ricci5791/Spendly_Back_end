2c1cef4ab45d65a689b1da25c2002c6314464bfc
ricci5791 <vadym.kolodiazhnyi@nure.ua>
2020-10-11 20:42:52 +0300

    change test view

--- spendly_api/views.py
+++ spendly_api/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def main_view(request):
-    return HttpResponse("<html><body>It is now </body></html>")
+    return HttpResponse(
+        "<html><body>Hello motherfakers. If you see this text i haven't wasted 3 hours of reading</body></html>")
