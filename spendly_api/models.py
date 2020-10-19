3402fc38c9299e1d4f17ca22d9031460dc816c3a
ricci5791 <htcone6759@gmail.com>
2020-10-19 14:12:45 +0300

    added api paths, core models, prototype of monobank webhook

--- spendly_api/models.py
+++ spendly_api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(max_length=50, primary_key=True)
+    password = models.CharField(max_length=30)
+
+    def get_
+
+
+class Account(models.Model):
+    acc_id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey('spendly_api.User', on_delete=models.CASCADE)
+    currency_code = models.IntegerField()
+    acc_type = models.CharField(max_length=10)
+
+
+class Transaction(models.Model):
+    account = models.ForeignKey('spendly_api.Account', on_delete=models.CASCADE)
+    transaction_id = models.CharField(max_length=30)
+    time_since_epoch = models.IntegerField()
+    desc = models.CharField(max_length=100)
+    balance = models.IntegerField()
+    amount = models.IntegerField()
+    merchant_category_code = models.IntegerField()
+    currency_code = models.IntegerField()
+
+
--- spendly_api/views.py
+++ spendly_api/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests as https
 
 
 # Create your views here.
 def main_view(request):
     return HttpResponse(
         "<html><body>Hello motherfakers. If you see this text i haven't wasted 3 hours of reading</body></html>")
+
+
+def init_response(request):
+    user_token = request.GET.get('token')
+    email = request.GET.get("email")
+    password = request.GET.get("password")
+    # TODO: add model logics to check user credentials
+
+    url = 'https://spendly-student.herokuapp.com/hook'
+    mono_url = 'https://api.monobank.ua/personal/webhook'
+
+    https.post(mono_url, json={"webHookUrl": url}, headers={"X-Token": user_token})
+
+    return HttpResponse()
+
+
+def monobank_webhook_response(request):
+    # TODO:
+    return None
