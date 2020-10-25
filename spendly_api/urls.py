7e35f57674bc8790b676c5f985356bd3cdae871f
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2020-10-25 18:14:40 +0200

    Update urls

--- spendly_api/urls.py
+++ spendly_api/urls.py
@@ -4,15 +4,14 @@ from rest_framework.authtoken.views import obtain_auth_token
 import spendly_api.views as views
 
 urlpatterns = [
-
     path('login', obtain_auth_token),
-    path('register', views.UserRegistrationView.as_view()),
+    path('register/', views.UserRegistrationView.as_view()),
 
-    path('webhook/set', views.MonobankIntegrationView.as_view()),
+
     path('webhook', views.MonobankWebhookView.as_view()),
+    path('webhook/set', views.MonobankIntegrationView.as_view()),
 
-    path('user/info', views.UserInformationView.as_view()),
-
-    path('user/transactions', views.UserTransactionsView.as_view()),
-    path('user/accounts', views.UserAccountsView.as_view()),
+    path('user/info/', views.UserInformationView.as_view()),
+    path('user/transactions/', views.UserTransactionsView.as_view()),
+    path('user/accounts/', views.UserAccountsView.as_view()),
 ]
